8e24a641d473363d96f3f3b177a7c2e7d2e051af
404nikhil <nicksd111@gmail.com>
2023-07-20 19:13:55 +0530

    recursion_a2z

--- notes and dry runs/recursion_time_complexity.cpp
+++ notes and dry runs/recursion_time_complexity.cpp
@@ -49,3 +49,133 @@ An ordinary array can be sorted as follows:
 int n = 7; // array size
 int a[] = {4,2,5,3,5,8,3};
 sort(a,a+n);
+
+
+Print name N times using recursion 
+
+name n times
+void f(i,n){
+if(i>n) return; // base case
+print("nikhil");
+f(i+1,N);
+}
+main()
+{
+int n;
+cin>>n;
+f(1,n);
+}
+
+1,3 to 2,3 to 3,3 to 4,3 base case returns 
+print(1-N)
+void() {
+if(i>n) return;
+print(i);
+f(i+1,n);
+}
+main() {
+int n;
+cin>>n;
+f(1,n);
+}
+
+
+
+Print 1 to N using backtracking recursion 
+by using backtraking 
+
+f(i,n){
+if(i<1)
+reutnr;
+f(i-1,n)
+print(i)
+}
+main(){
+int n;
+cin n;
+f(n,n);
+}
+
+here what we do is we backtrack our input by making our stack to mandatorily call the prev function
+
+PRINT N TO 1 using recursion 
+
+f(i,n) {
+uf(i<1) return;
+print(i)
+f(i-1,N); // main change only change
+}
+main(){
+int n;
+cin>>n;
+f(n,n)
+}
+
+Factorial of N number
+
+fact(int n){
+if(n==0){
+return 1;
+}
+return n * fact(n-1);
+}
+main
+{
+n
+print(fact(int n));
+}
+
+REVERSE AN ARRAY 
+
+void f(int i, int arr[], int n) {
+if(i>= n/2) return;
+swap(Arr[i], arr[n-i-1]);
+f(i+1, arr,  n);
+}
+
+int main() {
+n;
+cin>>n;
+int arr[n];
+for(int i=0; i<n; i++) cin>>arr[i];
+f(0, arr, n)
+for(int i=0; i<n; i++) court<<Arr[i]<<" ";
+}
+
+PALINDROME 
+
+a string on reversal reads the same
+
+madam, nitin 
+
+f(I) {
+if(I>+n/2) return true;
+if(s[i] != s[n-i-1]);
+return false;
+return f(i+1, s)
+}
+main(){
+string s
+court<< f(o,s)
+}
+
+
+FIBONNACI 
+
+multiple recursion calls
+fibonacci 
+
+0 1 1 2 3 5 8 13 21 34 ....
+
+nth fibonacci number
+
+f(5) = f(4) + f(3)
+
+simple approach
+
+f(0)=0 f(1)=1
+for( i =2; i<n i++){
+f[i] = f[i-1] + f[i-2]
+}
+
+by recursion calls
